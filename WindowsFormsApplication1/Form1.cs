7d42cc6219fec41a67eee2ef8846e7b679e2f3d8
Rida-Tech <reda.adams@gmail.com>
2020-05-19 16:49:04 +0000

    ze

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApplication1
         private void Form1_Load(object sender, EventArgs e)
         {
             label1.Text = "HAMZA OUROURNE";
+            label1.Text = "0000";
             
         }
     }
