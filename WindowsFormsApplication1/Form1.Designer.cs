7412c75d780f3d4a030f0775fe8f6565eafee554
Rida-Tech <reda.adams@gmail.com>
2020-05-19 17:54:46 +0000

    test

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -93,6 +93,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
