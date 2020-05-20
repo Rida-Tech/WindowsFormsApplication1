b1b66c7951d658ef9a23952336fdd3fe742752b8
Rida-Tech <reda.adams@gmail.com>
2020-05-20 15:15:45 +0000

    button

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -38,12 +38,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 90);
+            this.label1.Location = new System.Drawing.Point(51, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 17);
             this.label1.TabIndex = 0;
@@ -54,7 +56,7 @@
             this.textBox1.Location = new System.Drawing.Point(128, 83);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(360, 22);
+            this.textBox1.Size = new System.Drawing.Size(428, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
@@ -62,7 +64,7 @@
             this.textBox2.Location = new System.Drawing.Point(128, 124);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(360, 22);
+            this.textBox2.Size = new System.Drawing.Size(428, 22);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
@@ -70,13 +72,13 @@
             this.textBox3.Location = new System.Drawing.Point(128, 163);
             this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(360, 22);
+            this.textBox3.Size = new System.Drawing.Size(428, 22);
             this.textBox3.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(48, 129);
+            this.label2.Location = new System.Drawing.Point(50, 124);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 17);
             this.label2.TabIndex = 4;
@@ -85,7 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(48, 168);
+            this.label3.Location = new System.Drawing.Point(50, 163);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 17);
             this.label3.TabIndex = 5;
@@ -93,7 +95,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 202);
+            this.button1.Location = new System.Drawing.Point(340, 202);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(216, 52);
@@ -104,7 +106,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(274, 259);
+            this.button2.Location = new System.Drawing.Point(340, 258);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(216, 52);
@@ -118,17 +120,37 @@
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(234, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(494, 58);
+            this.label4.Size = new System.Drawing.Size(488, 57);
             this.label4.TabIndex = 8;
             this.label4.Text = "GESTION DES ELEVES";
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(512, 83);
+            this.listView1.Location = new System.Drawing.Point(562, 83);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(433, 228);
+            this.listView1.Size = new System.Drawing.Size(383, 228);
             this.listView1.TabIndex = 9;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(118, 203);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 52);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Modifier";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 259);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(216, 52);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Supprimer";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -136,6 +158,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGreen;
             this.ClientSize = new System.Drawing.Size(957, 390);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
@@ -167,6 +191,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
