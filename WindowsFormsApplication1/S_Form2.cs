359134b72a5d288184bbfa3d17e5e9a9c2f2eab6
Soka <sokaina.fadel@gmail.com>
2020-06-08 14:52:31 +0200

    ajouter button label

--- WindowsFormsApplication1/S_Form2.cs
+++ WindowsFormsApplication1/S_Form2.cs
@@ -10,21 +10,16 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class S_Form2 : Form
     {
-        public Form1()
+        public S_Form2()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "rida";
+            label2.Text = textBox1.Text;
         }
     }
 }
